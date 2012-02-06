02b98145b43ea95ed655307659128fa41befd7fb
sentientwaffle <sentientwaffle@gmail.com>
2012-02-06 15:12:34 -0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -9,3 +9,6 @@ cp -r _site/* _deploy
 touch _deploy/.nojekyll
 
 (cd _deploy && git add --all && git commit -am "deploy" && git push origin master)
+
+# git remote rm origin
+# git remote add origin git@github.com:stratuseditor/stratuseditor.github.com.git
